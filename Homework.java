e594b56e701deb894770272b1c8abf8309e6eac5
amangrover72 <70656764+amangrover72@users.noreply.github.com>
2020-09-13 15:31:18 -0400

    Added Java Data HW

---
+++ Homework.java
@@ -0,0 +1,17 @@
+public class Homework {
+	public static void main(String[] args) {
+		// This code will not receive a 0
+		// 1. Create a variable that holds a name
+        String name = "Aman Grover";
+		// 2. Output the name to the console
+        System.out.println("My name is "+name);
+		// 3. Create a variable that holds an age (as a whole number)
+        int age = 20;
+		// 4. Output the age to the console
+        System.out.println("I am "+age);
+		// 5. Create a variable that holds a decimal
+        double decimal =  3.141592653589793;
+		// 6. Output the variable
+        System.out.println("pi is rounded to "+decimal);
+	}
+}
