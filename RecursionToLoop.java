0a6415c562efcb674e6a247eb38d36aafc50f3fc
amangrover72 <70656764+amangrover72@users.noreply.github.com>
2020-09-30 21:44:18 -0400

    Converted Recursion to Loop

--- RecursionToLoop.java
+++ RecursionToLoop.java
@@ -1,10 +1,12 @@
 public class RecursionToLoop {
   
 	public static int sum(int num) {
-		if (num > 0) {
-			return num + sum(num - 1);
+        int sum = 0;
+        while (num > 0) {
+            sum += num;
+            num--;
 		}
-		return 0;
+		return sum;
 	}
 
 	public static void main(String[] args) {
